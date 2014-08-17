16658b28c3f499f02c8813dbeeb49364c5bd08e5
Jan Rabe <jan.rabe@wooga.net>
2014-08-17 17:00:32 +0200

    init

---
+++ Assets/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class Rotate : MonoBehaviour {
+
+        public void Update()
+        {
+            if (!Input.GetMouseButton(0)) return;
+            var x = -Input.GetAxis("Mouse X");
+            var y = -Input.GetAxis("Mouse Y");
+            float speed = 10;
+            transform.rotation *= Quaternion.AngleAxis(x * speed, Vector3.down);
+            transform.rotation *= Quaternion.AngleAxis(-y * speed, Vector3.left);
+        }
+    }
+}
